7c3e454755e6d9a8dfb2537fa2f2a10fc6b74655
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-14 15:26:22 -0400

    add features

---
+++ Catechism-App/ViewControll/Controller/PersonTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PersonTableViewCell.swift
+//  Catechism-App
+//
+//  Created by David Gonzalez on 3/12/22.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var textLabel2: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Catechism-App/ViewControll/Controller/ViewController.swift
+++ Catechism-App/ViewControll/Controller/ViewController.swift
@@ -6,14 +6,85 @@
 //
 
 import UIKit
+import SQLite3
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var personTable: UITableView!
+  
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let cellReuseIdentifier = "cell"
+    
+    var db:DBHelper = DBHelper()
+    
+    var persons:[Person] = []
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
+        cell.textLabel?.text = "Name: " + persons[indexPath.row].name + ", " + "Age: " + String(persons[indexPath.row].age)
+        return cell
+    }
+    
+    @IBOutlet weak var tableViewEmail: UITextField!
+    @IBOutlet weak var tableViewName: UITextField!
+    @IBOutlet weak var tableViewRol: UITextField!
+    
+    @IBOutlet weak var buttonSave: UIButton!
+    
+    
+    
+    @IBAction func bSave(_ sender: Any) {
+        //getting values from textfields
+        
+        
+        //displaying a success message
+        print("Person saved successfully")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        insertData()
+        populateValues()
+        
+      
+  
     }
-
-
+    
+    
+    
+    
+    
+    //MARK FUNCTIONS
+    
+    private func populateValues() {
+        textLabel.text = "Hello world!"
+        currencyLabel.text = "10000"
+        dateLabel.text = "07/08/2016"
+        imageView.image = UIImage(named: "flag")
+    }
+    
+    func insertData(){
+        personTable.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        personTable.delegate = self
+        personTable.dataSource = self
+        
+        db.deleteByID(id: 1)
+        db.deleteByID(id: 2)
+        db.insert(id: 11, name: "Pablo", age: 32)
+        db.insert(id: 12, name: "Young", age: 25)
+        db.insert(id: 13, name: "David", age: 23)
+        persons = db.read()
+    }
+    
 }
 
