d0d2f4b26ccac5de83e8794635523099a23e2c71
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-15 14:39:42 -0400

    create strucure

---
+++ Catechism-App/ViewControll/Controller/AdmonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdmonViewController.swift
+//  Catechism-App
+//
+//  Created by David Gonzalez on 3/15/22.
+//
+
+import UIKit
+
+class AdmonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
