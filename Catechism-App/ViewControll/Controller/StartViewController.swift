0c74660a58d64407529a659fa19c0c1ab1fa9e14
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-16 06:20:32 -0400

    add   FBSDKCoreKit ( Pods), use new project workspace file

--- Catechism-App/ViewControll/Controller/StartViewController.swift
+++ Catechism-App/ViewControll/Controller/StartViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, LoginButtonDelegate {
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+       
+    }
+    
    
     
     @IBOutlet weak var showP: UIButton!
@@ -24,12 +29,18 @@ class StartViewController: UIViewController {
     
     @IBOutlet weak var signUpB: UIButton!
     
+    
+    @IBOutlet weak var lgbtn: FBLoginButton!
+    
+    
     var loginChecked = false
     var signupChecked = true
     var email = ""
     var password = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        faceLogin()
         //setupUI()
         loginB.layer.cornerRadius = 20
         signUpB.layer.cornerRadius = 20
@@ -37,6 +48,63 @@ class StartViewController: UIViewController {
         userEmailText.text = email
         userPasswordText.text = password
         
+    }
+    
+    func faceLogin(){
+        var retUser:String = ""
+        if let token = AccessToken.current   , !token.isExpired{
+            let token =  token.tokenString
+                
+                let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,first_name"],
+                                             tokenString: token, version: nil, httpMethod: .get)
+                
+                req.start{
+                    conne, result, error in
+                    if let userdata = result as? [String:AnyObject] {
+                                                                                   retUser = (userdata["email"] as? String)!
+                                                                                         }
+                    self.userEmailText.text! = retUser
+                    print("resulados::::::",result)
+                }
+            }
+            
+            
+        else{
+//            let loginbtn = FBLoginButton()
+//            loginbtn.center = view.center
+//            view.addSubview(loginbtn)
+            self.lgbtn.delegate = self
+            lgbtn.permissions = ["public_profile","email"]
+            
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // *********
+    
+    
+    
+    
+    // *************
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?){
+        var retUser:String = ""
+     //   let token = result?.token?.to
+        
+    let token =  result?.token?.tokenString
+        
+        let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,name"],
+                                     tokenString: token, version: nil, httpMethod: .get)
+        
+        req.start{
+            conne, result, error in
+            if let userdata = result as? [String:AnyObject] {
+                                                                           retUser = (userdata["email"] as? String)!
+                                                                                 }
+            self.userEmailText.text! = retUser
+            print("userrrrrrrrrr:",result,"retUser:",retUser)
+        }
     }
     
 
--- Catechism-App/ViewControll/Controller/ViewController.swift
+++ Catechism-App/ViewControll/Controller/ViewController.swift
@@ -1,90 +1,69 @@
 //
 //  ViewController.swift
-//  Catechism-App
+//  FacebookDemo-class-1
 //
-//  Created by Dream Team Pablo De La Cruz on 3/12/22.
+//  Created by David Gonzalez on 3/14/22.
 //
 
 import UIKit
-import SQLite3
+import FBSDKLoginKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var personTable: UITableView!
-  
-    @IBOutlet weak var currencyLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+class ViewController: UIViewController, LoginButtonDelegate {
+ 
     
-    @IBOutlet weak var textLabel: UILabel!
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    let cellReuseIdentifier = "cell"
-    
-    var db:DBHelper = DBHelper()
-    
-    var persons:[Person] = []
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return persons.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
-        cell.textLabel?.text = "Name: " + persons[indexPath.row].name + ", " + "Age: " + String(persons[indexPath.row].age)
-        return cell
-    }
-    
-    @IBOutlet weak var tableViewEmail: UITextField!
-    @IBOutlet weak var tableViewName: UITextField!
-    @IBOutlet weak var tableViewRol: UITextField!
-    
-    @IBOutlet weak var buttonSave: UIButton!
-    
-    
-    
-    @IBAction func bSave(_ sender: Any) {
-        //getting values from textfields
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton){
         
-        
-        //displaying a success message
-        print("Person saved successfully")
     }
+
+    
+    @IBOutlet weak var lgbtn: FBLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        insertData()
-        populateValues()
         
-      
-  
-    }
-    
-    
-    
-    
-    
-    //MARK FUNCTIONS
-    
-    private func populateValues() {
-        textLabel.text = "Hello world!"
-        currencyLabel.text = "10000"
-        dateLabel.text = "07/08/2016"
-        imageView.image = UIImage(named: "flag")
-    }
-    
-    func insertData(){
-        personTable.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
-        personTable.delegate = self
-        personTable.dataSource = self
+        if let token = AccessToken.current   , !token.isExpired{
+            let token =  token.tokenString
+                
+                let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,first_name"],
+                                             tokenString: token, version: nil, httpMethod: .get)
+                
+                req.start{
+                    conne, result, error in
+                    print(result)
+                }
+            }
+            
+            
+        else{
+//            let loginbtn = FBLoginButton()
+//            loginbtn.center = view.center
+//            view.addSubview(loginbtn)
+            self.lgbtn.delegate = self
+            lgbtn.permissions = ["public_profile","email"]
+            
+        }
         
-        db.deleteByID(id: 1)
-        db.deleteByID(id: 2)
-        db.insert(id: 11, name: "Pablo", age: 32)
-        db.insert(id: 12, name: "Young", age: 25)
-        db.insert(id: 13, name: "David", age: 23)
-        persons = db.read()
+        
+        
+        // Do any additional setup after loading the view.
     }
     
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?){
+
+     //   let token = result?.token?.to
+        
+    let token =  result?.token?.tokenString
+        
+        let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,name"],
+                                     tokenString: token, version: nil, httpMethod: .get)
+        
+        req.start{
+            conne, result, error in
+            print(result)
+        }
+    }
 }
+
+
 
