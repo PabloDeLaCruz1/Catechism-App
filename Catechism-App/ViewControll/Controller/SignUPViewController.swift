27bee94a8f795975b73620267c23b4162e5ea57f
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-17 11:08:25 -0400

    add  Questions Class, DHHelper ( create table question, getData questions)

--- Catechism-App/ViewControll/Controller/SignUPViewController.swift
+++ Catechism-App/ViewControll/Controller/SignUPViewController.swift
@@ -20,8 +20,12 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var subscriptionType: UITextField!
     @IBOutlet weak var id: UITextField!
     
+    @IBOutlet weak var hidePB: UIButton!
+    @IBOutlet weak var showPB: UIButton!
+    @IBOutlet weak var subB: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        subB.layer.cornerRadius = 20
   print("selectedName:",userWellcome)
         // Do any additional setup after loading the view.
     }
@@ -29,12 +33,50 @@ class SignUpViewController: UIViewController {
 
    
     @IBAction func crearUsuario(_ sender: UIButton) {
-        saveUserDB()
+        if (emailText.text! != ""){
+            saveUserDB()
+            goNextView(nameView : "login")
+        }else{
+            print("Ingrese usuario")
+        }
+   
+    }
+    
+    @IBAction func ShowPass(_ sender: Any) {
+        print("Change passs")
+        passT.isSecureTextEntry = false
+        hidePB.isHidden = false
+        showPB.isHidden = true
+    }
+    
+    @IBAction func hidePass(_ sender: Any) {
+        print("Hide Change passs")
+        passT.isSecureTextEntry = true
+        hidePB.isHidden = true
+        showPB.isHidden = false
     }
     
     
+  
+    
+    // MARK  FUNCTIONS
     func saveUserDB(){
         db.insertUsers(id: Int(id.text!) ?? 0, name: emailText.text!, password: passT.text! , subscriptionType: Int(subscriptionType.text!) ?? 0)
+        
+        
     }
 
+    
+    func goNextView(nameView : String) {
+        if (nameView == "login") {
+            
+            let displayVC : StartViewController  = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "loginView") as!  StartViewController
+            
+           // displayVC.userWellcome   = userName.text!
+                    
+                self.present(displayVC, animated: true, completion: nil)
+        }
+    }
+    
+    
 }
