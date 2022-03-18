08844fad5f1aa8521efd103219a3a4cbb91274a5
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-18 00:06:22 -0400

    fixed and tested to make sure conflicts didnt bother others

--- Catechism-App/ViewControll/Controller/SignUPViewController.swift
+++ Catechism-App/ViewControll/Controller/SignUPViewController.swift
@@ -10,30 +10,30 @@ import SwiftUI
 
 class SignUpViewController: UIViewController {
 
-    var db:DBHelper = DBHelper()
+    var db: DBHelper = DBHelper()
     var userWellcome = ""
-    
+
     @IBOutlet weak var emailText: UITextField!
-   
+
 
     @IBOutlet weak var passT: UITextField!
-    
+
     @IBOutlet weak var subscriptionType: UITextField!
     @IBOutlet weak var id: UITextField!
-    
+
     @IBOutlet weak var hidePB: UIButton!
     @IBOutlet weak var showPB: UIButton!
     @IBOutlet weak var subB: UIButton!
     @IBOutlet weak var errorB: UILabel!
-    
+
     // Suscription
     @IBOutlet weak var freeeB: UIButton!
     @IBOutlet weak var paidB: UIButton!
     @IBOutlet weak var adminB: UIButton!
-  
-    
-    
-    
+
+
+
+
     struct ContentView: View {
         @State private var showingConfirmation = false
         @State private var backgroundColor = Color.white
@@ -43,113 +43,107 @@ class SignUpViewController: UIViewController {
                 .frame(width: 300, height: 300)
                 .background(backgroundColor)
                 .onTapGesture {
-                    showingConfirmation = true
-                }
+                showingConfirmation = true
+            }
         }
     }
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         subB.layer.cornerRadius = 20
-        
-     
-        
+
+
+
         //  print("selectedName:",userWellcome)
         // Do any additional setup after loading the view.
     }
-    
+
 
     @IBAction func BFree(_ sender: Any) {
         subscriptionType.text = "0"
-       print("free")
+        print("free")
     }
-    
-    
+
+
     @IBAction func BPaid(_ sender: Any) {
         subscriptionType.text = "1"
         print("paid")
     }
-    
-    
+
+
     @IBAction func BAdmin(_ sender: Any) {
         subscriptionType.text = "2"
         print("admin")
     }
-    
-   
+
+
     @IBAction func crearUsuario(_ sender: UIButton) {
-       
-        
-        if validateData(){
+
+        if validateData() {
             saveUserDB()
-            goNextView(nameView : "login")
-        }else{
+            goNextView(nameView: "login")
+        } else {
             print("Input user")
         }
-   
+
     }
-    
+
     @IBAction func ShowPass(_ sender: Any) {
         print("Change passs")
         passT.isSecureTextEntry = false
         hidePB.isHidden = false
         showPB.isHidden = true
     }
-    
+
     @IBAction func hidePass(_ sender: Any) {
         print("Hide Change passs")
         passT.isSecureTextEntry = true
         hidePB.isHidden = true
         showPB.isHidden = false
     }
-    
-    
-  
-    
+
     // MARK  FUNCTIONS
-    func saveUserDB(){
-        db.insertUsers(name: emailText.text!, password: passT.text! , subscriptionType: Int(subscriptionType.text!) ?? 0)
-
-
-    // MARK:  FUNCTIONS
-    
-    func validateData() -> Bool{
-        if emailText.text! == "" {
-            errorB.text = "Enter the user"
-            return false
-        }
-        if passT.text! == "" {
-            errorB.text = "Enter the password"
-            return false
-        }
-        
-        if subscriptionType.text! == "" {
-            errorB.text = "Enter Suscription"
-            return false
-        }
-        
-        if id.text! == "" {
-            errorB.text = "Enter id"
-            return false
-        }
-        
-        return true
+    func saveUserDB() {
+        db.insertUsers(name: emailText.text!, password: passT.text!, subscriptionType: Int(subscriptionType.text!) ?? 0)
     }
-     
 
+        // MARK:  FUNCTIONS
 
-    
-    func goNextView(nameView : String) {
-        if (nameView == "login") {
-            
-            let displayVC : StartViewController  = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "loginView") as!  StartViewController
-            
-           // displayVC.userWellcome   = userName.text!
-                    
+        func validateData() -> Bool {
+            if emailText.text! == "" {
+                errorB.text = "Enter the user"
+                return false
+            }
+            if passT.text! == "" {
+                errorB.text = "Enter the password"
+                return false
+            }
+
+            if subscriptionType.text! == "" {
+                errorB.text = "Enter Suscription"
+                return false
+            }
+
+            if id.text! == "" {
+                errorB.text = "Enter id"
+                return false
+            }
+
+            return true
+        }
+
+        func goNextView(nameView: String) {
+            if (nameView == "login") {
+
+                let displayVC: StartViewController = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "loginView") as! StartViewController
+
+                // displayVC.userWellcome   = userName.text!
+
                 self.present(displayVC, animated: true, completion: nil)
+            }
         }
+
+
     }
-    
-    
-}
+
--- Catechism-App/ViewControll/Controller/StartViewController.swift
+++ Catechism-App/ViewControll/Controller/StartViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 import Speech
 
 class StartViewController: UIViewController {
-    
-    
+
     @IBOutlet weak var showP: UIButton!
     @IBOutlet weak var hidePas: UIButton!
     @IBOutlet weak var userText: UITextField!
@@ -19,23 +18,21 @@ class StartViewController: UIViewController {
     @IBOutlet weak var userPasswordText: UITextField!
     @IBOutlet weak var loginB: UIButton!
     @IBOutlet weak var signUpB: UIButton!
-    
+
     var loginChecked = false
     var signupChecked = true
     var email = ""
     var password = ""
-    
-    
+
     var isStart = false
     let audioEng = AVAudioEngine()
     let req = SFSpeechAudioBufferRecognitionRequest()
     let speechR = SFSpeechRecognizer()
-    var rTask : SFSpeechRecognitionTask!
-    
+    var rTask: SFSpeechRecognitionTask!
+
     @IBOutlet weak var label: UILabel!
-    
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //setupUI()
@@ -44,9 +41,9 @@ class StartViewController: UIViewController {
         // Do any additional setup after loading the view.
         userText.text = email
         userPasswordText.text = password
-        
+
     }
-    
+
     @IBAction func showPass(_ sender: Any) {
         print("Change passs")
         userPasswordText.isSecureTextEntry = false
@@ -58,98 +55,97 @@ class StartViewController: UIViewController {
         userPasswordText.isSecureTextEntry = true
         hidePas.isHidden = true
         showP.isHidden = false
-        
+
     }
-    
+
     // *********
-    
+
     @IBAction func signUpB(_ sender: Any) {
         goNextView(nameView: "signUp")
     }
-    
-    func goNextView(nameView : String){
+
+    func goNextView(nameView: String) {
         if (nameView == "signUp") {
-            
-            let displayVC : SignUpViewController  = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "signUpSB") as!  SignUpViewController
-            
-            displayVC.userWellcome   = userText.text!
-            
+
+            let displayVC: SignUpViewController = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "signUpSB") as! SignUpViewController
+
+            displayVC.userWellcome = userText.text!
+
             self.present(displayVC, animated: true, completion: nil)
-        }else{
+        } else {
             print("no")
         }
     }
-    
-    
-    
-    @IBAction func loginButton(_ sender: Any) {
-        //DBHELPER
-        let data = DBHelper.init().getUsers() //  .inst.read()
-        for d in data{
-    @IBAction func activeMicro(_ sender: Any) {
-        isStart = !isStart
-        if isStart {
-            startSpeechRec()
-            micro.setTitle("stop", for: .normal)
-            label.text = ""
-            
-            micro.tintColor = .blue
-            
-         //   sender.setTitle("stop", for: .normal)
-        }else{
-            cancellSpeechRec()
-            micro.setTitle("start", for: .normal)
-            micro.tintColor = .red
-          //  sender.setTitle("start", for: .normal)
-        }
-    }
-    
-    
-    func cancellSpeechRec(){
+
+//    @IBAction func loginButton(_ sender: Any) {
+//        //DBHELPER
+//        let data = DBHelper.init().getUsers() //  .inst.read()
+//        for d in data {
+//            @IBAction func activeMicro(_ sender: Any) {
+//                isStart = !isStart
+//                if isStart {
+//                    startSpeechRec()
+//                    micro.setTitle("stop", for: .normal)
+//                    label.text = ""
+//
+//                    micro.tintColor = .blue
+//
+//                    //   sender.setTitle("stop", for: .normal)
+//                } else {
+//                    cancellSpeechRec()
+//                    micro.setTitle("start", for: .normal)
+//                    micro.tintColor = .red
+//                    //  sender.setTitle("start", for: .normal)
+//                }
+//            }
+//        }
+//    }
+
+    func cancellSpeechRec() {
         rTask.finish()
         rTask.cancel()
         rTask = nil
         req.endAudio()
         audioEng.stop()
-        
+
         if audioEng.inputNode.numberOfInputs > 0 {
             audioEng.inputNode.removeTap(onBus: 0)
         }
         print("cancel")
     }
-    
-    
-    
-    
+
+
+
+
     @IBAction func loginButton(_ sender: Any) {
-        
-        if validateData(){
+
+        if validateData() {
             // ***
             //DBHELPER
-            let data = DBHelper.init().read() //  .inst.read()
-            for d in data{
-                
-                if (d.name == userText.text!) &&  (userText.text! != "") {
-                    if  (d.password ==  userPasswordText.text!){
-                        let displayVC : WellcomeViewController = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "WellcomeSB") as! WellcomeViewController
-                        displayVC.userWellcome   = userText.text!
-                        displayVC.susWellcome    = String(d.subscriptionType)
-                        
+            let data = DBHelper.init().getUsers() //  .inst.read()
+            for d in data {
+
+                if (d.name == userText.text!) && (userText.text! != "") {
+                    if (d.password == userPasswordText.text!) {
+                        let displayVC: WellcomeViewController = UIStoryboard(name: "StartStoryboard", bundle: nil).instantiateViewController(withIdentifier: "WellcomeSB") as! WellcomeViewController
+                        displayVC.userWellcome = userText.text!
+                        displayVC.susWellcome = String(d.subscriptionType)
+
                         self.present(displayVC, animated: true, completion: nil)
-                        print("Exxiste  id:***********************:", d.id, "user: ",d.name,"pass:",d.password , d.subscriptionType)
-                    }  // ****
-                    else{
+                        print("Exxiste  id:***********************:", d.id, "user: ", d.name, "pass:", d.password, d.subscriptionType)
+                    } // ****
+                        else {
                         error.text = "Password not valid"
                     }
                 }
-                else{
+                else {
                     error.text = "User not valid"
                 }
             }
-        }  // *******
+        } // *******
     }
-    
-    func validateData() -> Bool{
+
+    func validateData() -> Bool {
         if userText.text! == "" {
             error.text = "Enter the user"
             return false
@@ -158,46 +154,46 @@ class StartViewController: UIViewController {
             error.text = "Enter the password"
             return false
         }
-        
+
         return true
     }
-    
+
     // MARK: SPEECH
-    func startSpeechRec(){
+    func startSpeechRec() {
         let nd = audioEng.inputNode
         let recordF = nd.outputFormat(forBus: 0)
         nd.installTap(onBus: 0, bufferSize: 1024, format: recordF)
-                      {
-            (buffer , _ ) in self.req.append(buffer)
+        {
+            (buffer, _) in self.req.append(buffer)
         }
         audioEng.prepare()
         do {
             try audioEng.start()
-        }catch let err {
+        } catch let err {
             printContent(err)
         }
-        
-        rTask  = speechR?.recognitionTask(with: req, resultHandler: {
+
+        rTask = speechR?.recognitionTask(with: req, resultHandler: {
             (resp, error) in
-            
-            guard let rsp = resp else{
+
+            guard let rsp = resp else {
                 print(error.debugDescription)
-                
+
                 return
             }
             let msg = resp?.bestTranscription.formattedString
             self.label.text = msg!
-            
-            
-            
-            var str : String = ""
-            for seg in  resp!.bestTranscription.segments{
+
+
+
+            var str: String = ""
+            for seg in resp!.bestTranscription.segments {
                 let indexTo = msg!.index(msg!.startIndex, offsetBy: seg.substringRange.location)
                 str = String(msg![indexTo...])
             }
-       
-             //*** reconigzer
-            switch str{
+
+            //*** reconigzer
+            switch str {
             case "Signup":
                 self.goNextView(nameView: "signUp")
             case "signup":
@@ -214,18 +210,15 @@ class StartViewController: UIViewController {
                 self.goNextView(nameView: "signUp")
             case "Go":
                 self.goNextView(nameView: "signUp")
-            default :
+            default:
                 print("No existe")
             }
-            
-            
-        
+
+
+
         })
-        
-        
+
+
         print("start")
     }
-    
 }
-
-
